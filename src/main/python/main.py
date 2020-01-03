ec54be29d816813551968e42a263ec422d474f99
orionbytes <orion.bytes@gmail.com>
2020-01-03 17:18:01 +0100

    add toolbar

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,5 +1,5 @@
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QToolBar
 
 import sys
 
@@ -7,6 +7,8 @@ if __name__ == '__main__':
     appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
     window = QMainWindow()
     window.resize(250, 150)
+    toolbar = QToolBar()
+    window.addToolBar(toolbar)
     window.show()
     exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
     sys.exit(exit_code)
