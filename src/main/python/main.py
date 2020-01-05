1f954c2f23fc8f7d97c4eb00d5f1d126f388cca5
orionbytes <orion.bytes@gmail.com>
2020-01-05 10:39:12 +0100

    show and hide

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -6,6 +6,7 @@ import bokeh
 from io import BytesIO
 import numpy as np
 import holoviews as hv
+import pyviz_comms
 
 if __name__ == '__main__':
     appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
@@ -28,6 +29,7 @@ if __name__ == '__main__':
 
     QtCore.qInstallMessageHandler(qt_message_handler)
 
+
     class Ui_Dialog(object):
         def setupUi(self, Dialog):
             Dialog.setObjectName("Dialog")
@@ -39,37 +41,40 @@ if __name__ == '__main__':
             self.buttonBox.setObjectName("buttonBox")
             self.checkBox = QtWidgets.QCheckBox(Dialog)
             self.checkBox.setGeometry(QtCore.QRect(10, 390, 85, 24))
-            self.checkBox.setObjectName("checkBox")
+            self.checkBox.setObjectName("checkBox1")
             self.checkBox_2 = QtWidgets.QCheckBox(Dialog)
             self.checkBox_2.setGeometry(QtCore.QRect(120, 390, 85, 24))
-            self.checkBox_2.setObjectName("checkBox_2")
+            self.checkBox_2.setObjectName("checkBox2")
             self.webView = QtWebEngineWidgets.QWebEngineView(Dialog)
             self.webView.setGeometry(QtCore.QRect(0, 0, 671, 381))
             self.webView.setUrl(QtCore.QUrl("about:blank"))
             self.webView.setObjectName("webView")
 
+            # Bokeh test
+            def bokeh_test():
+                if self.webView.isHidden():
+                    self.webView.show()
+                else:
+                    curve = hv.Curve(range(10))
+                    img = hv.Image(np.random.rand(10, 10))
+                    buf = BytesIO()
+                    hv.save(curve + img, buf, backend='bokeh', fmt='html')
+                    self.html = buf.read().decode()
+                    self.webView.setHtml(self.html)
+                    self.webView.adjustSize()
+
             self.retranslateUi(Dialog)
             self.buttonBox.accepted.connect(Dialog.accept)
             self.buttonBox.rejected.connect(Dialog.reject)
-            self.checkBox.stateChanged['int'].connect(Dialog.showNormal)
-            self.checkBox_2.stateChanged['int'].connect(Dialog.showMaximized)
+            self.checkBox.stateChanged['int'].connect(self.webView.hide)
+            self.checkBox_2.stateChanged['int'].connect(bokeh_test)
             QtCore.QMetaObject.connectSlotsByName(Dialog)
-
-            # Bokeh test
-            curve = hv.Curve(range(10))
-            img = hv.Image(np.random.rand(10, 10))
-            buf = BytesIO()
-            hv.save(curve + img, buf, backend='bokeh', fmt='html')
-            self.html = buf.read().decode()
-
-            self.webView.setHtml(self.html)
-
 
         def retranslateUi(self, Dialog):
             _translate = QtCore.QCoreApplication.translate
             Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-            self.checkBox.setText(_translate("Dialog", "CheckBox"))
-            self.checkBox_2.setText(_translate("Dialog", "CheckBox"))
+            self.checkBox.setText(_translate("Dialog", "Hide"))
+            self.checkBox_2.setText(_translate("Dialog", "Show"))
 
 
     app = QtWidgets.QApplication(sys.argv)
